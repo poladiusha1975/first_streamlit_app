686c6a899444d0bf2b05b50db48e78ee1850dcb1
poladiusha1975 <125676385+poladiusha1975@users.noreply.github.com>
2023-02-21 00:18:49 +0530

    Update Mstreamlit_app_py.py

--- Mstreamlit_app_py.py
+++ Mstreamlit_app_py.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My parents newly diner')
+
 streamlit.header('Breakfast')
 streamlit.header('Oat Meal')
 streamlit.header('Cereals')
 streamlit.header('coockies')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
