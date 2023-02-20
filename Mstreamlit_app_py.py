e629cc5afc94dc81ff1350d4fa608a53e3ab17d1
poladiusha1975 <125676385+poladiusha1975@users.noreply.github.com>
2023-02-21 00:22:13 +0530

    Update Mstreamlit_app_py.py

--- Mstreamlit_app_py.py
+++ Mstreamlit_app_py.py
@@ -1,5 +1,5 @@
 import streamlit
-import Pandas
+
 streamlit.title('My parents newly diner')
 
 streamlit.header('Breakfast')
@@ -7,5 +7,7 @@ streamlit.header('Oat Meal')
 streamlit.header('Cereals')
 streamlit.header('coockies')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import Pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
